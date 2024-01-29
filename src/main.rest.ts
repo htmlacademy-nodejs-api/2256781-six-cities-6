2d98f919353c272c9ce037634ce025ecddd8356f
Dimaggio <zodiak-d@yandex.ru>
2024-01-29 11:01:12 +0300

    проверка работоспособности класса RestConfig
    ```
    PORT=5000 npm run start:dev
    ```

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,8 +1,11 @@
 import { PinoLogger } from './shared/libs/index.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from './shared/libs/index.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
+  const config = new RestConfig(process.env);
+  console.log(config.get('PORT'));
 
   const application = new RestApplication(logger);
   await application.init();
