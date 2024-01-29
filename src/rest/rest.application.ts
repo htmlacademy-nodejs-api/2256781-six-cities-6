b2e92e12236fccbe9d8e6b77e94536f179a5f35b
Dimaggio <zodiak-d@yandex.ru>
2024-01-29 23:11:32 +0300

    Внедрил зависимости в RestApplication
    нужно не забыть, в точке входа приложения импортировать `reflect-metadata`.
    Иначе `inversify` не сможет работать и извлекать мета информацию

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,10 +1,13 @@
+import { inject, injectable } from 'inversify';
 import { TRestSchema } from '../shared/libs/config/rest.schema.js';
 import { IConfig, ILogger } from '../shared/libs/index.js';
+import { Component } from '../shared/types/component.enum.js';
 
+@injectable()
 export class RestApplication {
   constructor(
-    private readonly logger: ILogger,
-    private readonly config: IConfig<TRestSchema>,
+    @inject(Component.Logger) private readonly logger: ILogger,
+    @inject(Component.Config) private readonly config: IConfig<TRestSchema>,
   ) { }
 
   public async init() {
