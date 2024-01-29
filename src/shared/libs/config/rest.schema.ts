cee2b1799eb48d38ea2dd69a1f3b948e5f05a355
Dimaggio <zodiak-d@yandex.ru>
2024-01-29 17:26:05 +0300

    Расширил тип RestSchema
    В ключе DB_HOST используется внешний форматер. Форматер (`format`) — функция, которая знает как проверить то или иное значение. Здесь `ipaddress`.
    
    `ipaddress` — это внешний форматер из пакета `convict-format-with-validator`

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -2,6 +2,8 @@ import convict from 'convict';
 
 export type TRestSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configRestSchema = convict<TRestSchema>({
@@ -10,5 +12,17 @@ export const configRestSchema = convict<TRestSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
   },
 });
