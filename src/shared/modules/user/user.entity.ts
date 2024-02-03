10b4fde6f5753253aae7d071aea65e3ea6d5d740
Dimaggio <zodiak-d@yandex.ru>
2024-02-03 12:07:58 +0300

    Добавил методы в UserEntity. WIP
    Пароль для пользователя будет устанавливаться с помощью метода `setPassword` - параметры у метода пароль и соль
    
    добавил функцию хелпер `createSHA256`, где юзается`createHmac` - ему
    нужно передать алгоритм хеширования и соль (используется алгоритм `sha256` - т.к. он быстрый и надёжный)

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,16 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, modelOptions, prop } from '@typegoose/typegoose';
 import { IUser, TUserType } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/hash.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base { }
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements IUser {
   @prop({ required: true, default: '' })
@@ -18,9 +25,27 @@ export class UserEntity extends defaultClasses.TimeStamps implements IUser {
   @prop({ unique: true, required: true })
   public email!: string;
 
+  // ? Вопрос: как его сделать private ?
   @prop({ required: true, default: '' })
   public password!: string;
 
+  constructor(userData: IUser) {
+    super();
+
+    this.name = userData.name;
+    this.type = userData.type;
+    this.avatarUrl = userData.avatarUrl;
+    this.email = userData.email;
+    this.password = userData.password;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
