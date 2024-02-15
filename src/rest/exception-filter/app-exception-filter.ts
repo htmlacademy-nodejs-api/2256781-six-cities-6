5107e2a632377ecf94641e4bd0c31ad6d3695f0f
Dimaggio <zodiak-d@yandex.ru>
2024-02-15 23:02:17 +0300

    Добавил класс <code>AppExceptionFilter</code>
    Теперь добавим имплементацию `ExceptionFilter`. Для этого создадим класс `AppExceptionFilter`.
    
    В классе нам потребуется логер. Поэтому внедрим его сразу в виде
    зависимости через конструктор. В конструкторе добавим вывод информации о регистрации `AppExceptionFilter`.
    
    Затем имплементируем метод `catch`. Пока сделаем самый простой вариант. Сделаем запись в лог сообщения ошибки (`error.message`) и отправим клиенту корректный ответ. Помните, даже если в сервисе произошла ошибка, следует отправить клиенту корректный код. В случае неизвестной ошибки таким кодом станет `500`.
    
    Помимо кода с ошибкой отправим объект с ошибкой. Здесь можно пойти дальше и внедрить отправку дополнительных констант для автоматической обработки ошибок клиентом, но пока не будем переусложнять.

---
+++ src/rest/exception-filter/app-exception-filter.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from 'inversify';
+import { StatusCodes } from 'http-status-codes';
+import { NextFunction, Request, Response } from 'express';
+import { IExceptionFilter } from './exception-filter.interface.js';
+import { ILogger } from '../../shared/libs/index.js';
+import { Component } from '../../shared/types/index.js';
+
+@injectable()
+export class AppExceptionFilter implements IExceptionFilter {
+  constructor(
+    @inject(Component.Logger) private readonly logger: ILogger
+  ) {
+    this.logger.info('Register AppExceptionFilter');
+  }
+
+  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+    this.logger.error(error.message, error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+}
