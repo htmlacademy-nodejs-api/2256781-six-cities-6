fee6eac00200e8169fe73d6af1e84801dd9a2d16
Dimaggio <zodiak-d@yandex.ru>
2024-02-16 17:48:33 +0300

    Добавил создание пользователей в `UserController`
    Последним шагом закончим с обработчиком `create` в `UserController`. Он отвечает за добавление новых пользователей в базу данных. Сначала проверим существование пользователя. Если он существует, то вернём код `409`. Обратите внимание, мы напрямую не вызываем `send`. Вместо этого бросаем ошибку типа `HttpError`.
    
    Если всё хорошо и пользователь создан, то вернём объект пользователя без поля «пароль». Структуру объекта для клиента предварительно описали в `UserDto`.
    
    Клиенту не нужны `createdAt`, `updatedAt`, `_id` и `__v`,
    решить эту задачу можно решить несколькими способами. Первый: модифицировать обработчик `toJSON` для модели. В `mongoose` реализована эта возможность (смотри документацию). Второй: определить RDO с нужными полями. Этот способ более гибкий. Воспользуемся им.
    
    RDO объявим в виде класса, а создадим его с помощью `plainToInstance` из пакета `class-transformer`. Эту функцию придётся использовать часто, поэтому подготовим обёртку в виде `fillDTO`. Обратите внимание, в `CategoryRdo` мы применяем декоратор `@Expose`. Им мы отмечаем, что свойство должно быть включено для заполнения.
    
    Пакет `class-transformer` позволяет создавать классы на основании объектов, а также выполнять обратную процедуру. Он пригодится для подготовки DTO

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,3 +1,5 @@
+import { ClassConstructor as TClassConstructor, plainToInstance } from 'class-transformer';
+
 export function generateRandomValue(min: number, max: number, numAfterDigit = 0) {
   return Number(((Math.random() * (max - min)) + min).toFixed(numAfterDigit));
 }
@@ -15,6 +17,10 @@ export function getRandomItem<T>(items: T[]): T {
 
 export function getErrorMessage(error: unknown): string {
   return error instanceof Error ? error.message : '';
+}
+
+export function fillDTO<T, V>(someDto: TClassConstructor<T>, plainObject: V) {
+  return plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
 }
 
 export function createErrorObject(message: string) {
--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,14 +1,21 @@
 import { inject, injectable } from 'inversify';
-import { Response as IResponse } from 'express';
-import { BaseController, HttpMethod } from '../../libs/index.js';
+import { Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { BaseController, HttpError, HttpMethod } from '../../libs/index.js';
 import { ILogger } from '../../libs/index.js';
-import { Component } from '../../types/index.js';
+import { Component, TConfigSchema } from '../../types/index.js';
 import { TCreateUserRequest } from './create-user-request.type.js';
+import { IUserService } from './user-service.interface.js';
+import { IConfig } from '../../libs/index.js';
+import { fillDTO } from '../../helpers/index.js';
+import { UserRdo } from './rdo/user.rdo.js';
 
 @injectable()
 export class UserController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: ILogger,
+    @inject(Component.UserService) private readonly userService: IUserService,
+    @inject(Component.Config) private readonly configService: IConfig<TConfigSchema>,
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
@@ -17,9 +24,20 @@ export class UserController extends BaseController {
   }
 
   public async create(
-    _req: TCreateUserRequest,
-    _res: IResponse,
+    { body }: TCreateUserRequest,
+    res: Response,
   ): Promise<void> {
-    throw new Error('[UserController] Oops');
+    const existsUser = await this.userService.findUnique({ email: body.email });
+
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» exists.`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.created(res, fillDTO(UserRdo, result));
   }
 }
