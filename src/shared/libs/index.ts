30a6b1b9829ce7b2cc9adb5c7df644b546a0df0b
Dimaggio <zodiak-d@yandex.ru>
2024-02-19 01:15:37 +0300

    Добавил первую middleware `ValidateObjectIdMiddleware`
    Попробуем решить практическую задачу с помощью middleware. Для
    получения информации по объявлению (или любой другой сущности) клиент
    должен отправить её идентификатор. Идентификатор (`ObjectId`) — это
    специально-подготовленная уникальная строка.
    
    Какие с ней сложности? Перед тем как получить сущность, мы делаем
    проверку на наличие. Если сущности нет, то возвращаем соответствующий
    код ошибки. Однако в этом месте есть проблема. Код проверки в
    `OfferController` нормально отработает, если клиент отравит валидный
    идентификатор. Идентификатор, который может быть преобразован к типу
    `ObjectId`. Если выполнить преобразование не получится, произойдёт
    ошибка.
    
    Мы можем делать дополнительную проверку на валидность идентификатора
    сущности в обработчике, но не очень эффективно. Так как передавать
    подобные идентификаторы придётся во многих местах. Вынесем эту проверку
    в middleware, которую затем подключим к нужным обработчикам.
    
    Создадим первую middleware `ValidateObjectIdMiddleware`. Она должна
    реализовать интерфейс `Middleware`. В методе `execute` выполним проверку с помощью метода `isValid` объекта `ObjectId`. Если проверка пройдена, вызовем функцию `next`, чтобы передать эстафетную палочку следующему обработчику. В противном случае бросим ошибку с кодом `400`.
    
    Поскольку параметр для передачи идентификатора может называться как
    угодно, предусмотрим передачу имени через конструктор. Для этого в middleware опишем приватное поле `param`.

--- src/shared/libs/index.ts
+++ src/shared/libs/index.ts
@@ -25,3 +25,4 @@ export * from './rest/types/request.params.type.js';
 export * from './rest/types/request-body.type.js';
 export * from './rest/errors/http-error.js';
 export * from './rest/middleware/middleware.interface.js';
+export * from './rest/middleware/validate-object-id.middleware.js';
