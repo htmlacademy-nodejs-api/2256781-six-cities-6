ae2546ecd96f78c9df8c5ee1a59eba5be570456f
Dimaggio <zodiak-d@yandex.ru>
2024-02-19 23:30:54 +0300

    Добавил middleware `UploadFileMiddleware`
    Обработка запроса с целью извлечения файлов выполним в отдельной
    middleware. Создадим `UploadFileMiddleware`. Не забудьте, middleware должна имплементировать интерфейс `Middleware`.
    
    В методе `execute` реализуем создание экземпляра `diskStorage`.
    
    `multer` абстрагируется от места хранения файлов. Для этого он предоставляет абстракцию в виде хранилища (`storage`). Из коробки доступно два варианта хранилищ: `diskStorage` и `memoryStorage`. Мы решили сохранять файлы на диске, поэтому воспользуемся первым вариантом.
    
    Создадим хранилище и заполним свойства `destination` и `filename`. В первом следует указать путь для сохранения файлов. Путь будем передавать через конструктор (`uploadDirectory`). А вот свойство `filename` более сложное. Ему мы можем задать функцию, в которой можно
    реализовать свой алгоритм для именования полученных файлов.
    
    По умолчанию `multer` сохраняет все файлы без расширения, а в качестве имени использует уникальную строку. Мы можем переопределить это поведение. Расширение получим на основании `file.mimetype`, а в качестве имени воспользуемся уникальной строкой, полученной с помощью `nanoid`.
    
    Последним шагом воспользуемся объектом `multer` и получим готовую middleware, сохраним её в `uploadSingleFileMiddleware``. Затем исполним.

--- src/shared/libs/index.ts
+++ src/shared/libs/index.ts
@@ -28,3 +28,4 @@ export * from './rest/middleware/middleware.interface.js';
 export * from './rest/middleware/validate-object-id.middleware.js';
 export * from './rest/middleware/validate-dto.middleware.js';
 export * from './rest/middleware/document-exists.middleware.js';
+export * from './rest/middleware/upload-file.middleware.js';
